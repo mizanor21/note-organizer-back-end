cf8a9f6bc284c8b0d7a8363b9055bd2516037745
mizan21 <mizanorrahman398@gmail.com>
2023-08-05 00:33:26 +0600

    Notes part added

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Node Server is running!");
+const uri =
+  "mongodb+srv://notesAdmin:6YjAX14mmtlWN4K6@cluster0.disah5t.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    const noteCollection = client.db("KeepNotesDB").collection("notes");
+
+    app.get("/", (req, res) => {
+      res.send("Node Server is running!");
+    });
+
+    app.get("/notes", async (req, res) => {
+      const query = {};
+      const notes = await noteCollection.find(query).toArray();
+      res.send(notes);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
