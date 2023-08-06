c54ae2ea60900f37304b1c80f9e49f0fc2b9e892
mizan21 <mizanorrahman398@gmail.com>
2023-08-06 13:16:13 +0600

    Some code changes

--- index.js
+++ index.js
@@ -43,6 +43,27 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/notes/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedNote = req.body;
+        const options = { upsert: true };
+
+        const result = await noteCollection.updateOne(
+          filter,
+          { $set: updatedNote },
+          options
+        );
+
+        res.send(result);
+        console.log(updatedNote);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred while updating the note.");
+      }
+    });
+
     app.delete("/note/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
